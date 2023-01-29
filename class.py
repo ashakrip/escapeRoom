890c294f322500c342ed7515cd9ef8b95b460f18
ashakrip <119749754+ashakrip@users.noreply.github.com>
2023-01-29 20:43:22 +0530

    Add files via upload
    A python based Escape Room game/program.
    Provides multi-choice routes to the player to complete tasks and fulfill the mission

---
+++ class.py
@@ -0,0 +1,24 @@
+# def get_ready():
+#     print("Get up")
+#     print("Brush my teeth")
+#     print("shower")
+#     print("Have breakfast")
+#     print("Leave home")
+#
+# get_ready()
+# get_ready()
+# get_ready()
+
+
+def make_pizza(toppings):
+    print("Get the dough")
+    print("Stretch it out 10 inches")
+    print("Add tomato sauce")
+    print(f"Add {toppings}")
+    print("Bake the pizza in the oven")
+
+make_pizza("pineapple")
+make_pizza("pepperoni")
+
+def start_game():
+
